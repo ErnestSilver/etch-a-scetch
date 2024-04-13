519c3cb9f6532e282dbb16848e97875a1312bf0a
Ernest Sribnyi <sribnyi.ernest2000@gmail.com>
2024-04-12 17:55:05 -0700

    Add grid create button, add size input for button

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const container = document.querySelector("div");
 
-for (let i = 0; i < 256; i++) {
-  const gridBox = document.createElement("div");
-  gridBox.style.width = "40px";
-  gridBox.style.height = "40px";
-  gridBox.style.border = "1px solid black";
-  container.appendChild(gridBox);
-}
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  let input = Number(prompt("How many boxes should grid side be made of?"));
+  console.log(input);
+  if (input > 100) {
+    alert("Maximum number of boxes on grid side is 100 ");
+    input = 0;
+  } else if (isNaN(input)) {
+    alert("Invalid input");
+  }
+  const boxSize = 660 / input;
+  for (let i = 0; i < input * input; i++) {
+    const gridBox = document.createElement("div");
+    gridBox.style.width = boxSize - 2 + "px";
+    gridBox.style.height = boxSize - 2 + "px";
+    gridBox.style.border = "1px solid black";
+    container.appendChild(gridBox);
+  }
+});
